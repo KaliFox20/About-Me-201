19c0d988f6fc50bf5107a8318002aa06a75aa2b7
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-07-12 10:53:15 -1000

    Changes to About-Me-201

--- js/app.js
+++ js/app.js
@@ -7,12 +7,13 @@ let answerZero = prompt('What is your name?');
 
 alert('Welcome ' + answerZero + '!');
 
-let score = 0;
+//let score = 0;
 let answerOne = prompt('Am I over 30 years old?').toLowerCase();
 
 if (answerOne === 'yes' || answerOne === 'y') {
   alert('You are correct!');
-  score++;
+  //console.log('You are correct!');
+ // score++;
 }
 else if (answerOne === 'no' || answerOne === 'n') {
   alert('Sorry I am older.');
@@ -22,7 +23,8 @@ let answerTwo = prompt('Did I ever serve in the Military?').toLowerCase();
 
 if (answerTwo === 'yes' || answerTwo === 'y') {
   alert('You are correct!');
-  score++;
+  //console.log('You are correct!');
+  //score++;
 }
 else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('Sorry that is incorrect.');
@@ -32,7 +34,8 @@ let answerThree = prompt('Do i play D&D').toLowerCase();
 
 if (answerThree === 'yes' || answerThree === 'y') {
   alert('You are correct!');
-  score++;
+  //console.log('You are correct!');
+  //score++;
 }
 else if (answerThree === 'no' || answerThree === 'n') {
   alert('Sorry that is incorrect.');
@@ -41,66 +44,68 @@ else if (answerThree === 'no' || answerThree === 'n') {
 let answerFour = prompt('Do I play VideoGames?').toLowerCase();
 if (answerFour === 'yes' || answerFour === 'y') {
   alert('You are correct!');
-  score++;
+  //console.log('You are correct!');
+  //score++;
 }
 else if (answerFour === 'no' || answerFour === 'n') {
   alert('Sorry that is incorrect.');
-  score++;
+  //score++;
 }
 
 let answerFive = prompt('Do I like to Hike?').toLowerCase();
 if (answerFive === 'yes' || answerFive === 'y') {
   alert('You are correct!');
-  score++;
+  //console.log('You are correct!');
+  //score++;
 }
 else if (answerFive === 'no' || answerFive === 'n') {
   alert('Sorry that is incorrect.');
 }
 
-let GuessingGame = prompt('How much Bacon do I have? Pick a numnber 1-100');
-let correctAnswer = 55;
-let attempts = 5;
+//let GuessingGame = prompt('How much Bacon do I have? Pick a numnber 1-100');
+//let correctAnswer = 55;
+//let attempts = 5;
 
-for (let i = 0; i < attempts; i++) {
-  while (GuessingGame < 1 || GuessingGame > 100) {
-    GuessingGame = prompt('Please try again...! Pick again.');
+//for (let i = 0; i < attempts; i++) {
+  //while (GuessingGame < 1 || GuessingGame > 100) {
+    //GuessingGame = prompt('Please try again...! Pick again.');
   }
-  if (GuessingGame == correctAnswer) {
-    alert('WE HAVE A WINNER!!');
-    document.write('The Champ!');
-    score++;
-    break;
-  }
-  else if (GuessingGame > correctAnswer) {
-    GuessingGame = prompt('Sorry too high. Try again.');
-  }
-  else if (GuessingGame < correctAnswer) {
-    GuessingGame = prompt('Sorry too low. Try again.');
-  }
-  if (i === 4) {
-    alert('sorry no dice.');
-  }
-}
+ // if (GuessingGame == correctAnswer) {
+   // alert('WE HAVE A WINNER!!');
+   // document.write('The Champ!');
+   // score++;
+   // break;
+  //}
+ // else if (GuessingGame > correctAnswer) {
+   // GuessingGame = prompt('Sorry too high. Try again.');
+ // }
+  //else if (GuessingGame < correctAnswer) {
+    //GuessingGame = prompt('Sorry too low. Try again.');
+  //}
+ // if (i === 4) {
+   // alert('sorry no dice.');
+ // }
+//}
 
 
-let MyArr = ['tasty', 'red', 'fruit'];
-let attemptsOne = 6;
+//let MyArr = ['tasty', 'red', 'fruit'];
+//let attemptsOne = 6;
 
-for (let t = 0; t < attemptsOne; t++) {
-  let GuessingGameOne = prompt('what is a apple').toLowerCase();
-  for (let a = 0; a < MyArr.length; a++) {
-    if (GuessingGameOne === MyArr[a]) {
-      alert('WE HAVE A WINNER!!');
-      score++;
-      t = 6;
-      break;
-    }
-  }
-}
-alert('answer were tasty, red, and fruit');
+//for (let t = 0; t < attemptsOne; t++) {
+  //let GuessingGameOne = prompt('what is a apple').toLowerCase();
+  //for (let a = 0; a < MyArr.length; a++) {
+   // if (GuessingGameOne === MyArr[a]) {
+     // alert('WE HAVE A WINNER!!');
+      //score++;
+      //t = 6;
+      //break;
+    //}
+ // }
+//}
+//alert('answer were tasty, red, and fruit');
 
-alert('score = '+ score + '/7');
+//alert('score = '+ score + '/7');
 
-}
+//}
 
 q6();
